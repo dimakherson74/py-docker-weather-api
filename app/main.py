9d193f67dba552e4e588b3c7f4b587d919302908
Dolgov Dmytro <dimakherson74@gmail.com>
2025-04-06 18:45:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ import os
 import requests
 
 
+URL = "http://api.weatherapi.com/?"
+
+
 def get_weather(city: str = "Paris") -> None:
     api_key = os.getenv("API_KEY")
     if not api_key:
         raise Exception("API_KEY not set in environment variables!")
-    URL = "http://api.weatherapi.com/?"
 
     url = f"{URL}/v1/current.json?key={api_key}&q={city}&aqi=no"
 
